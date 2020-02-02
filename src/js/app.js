19ce8549479681514b7fd7d769acb1e9170b37ec
earthtone82 <antonio.hubilla@pizzahutaustralia.com.au>
2020-02-02 12:48:47 +1100

    Update Static Code Analysis
    - add prop-types validation
    - add eslint-plugin-react

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Heading from './components/Heading.js'
 
-export default function App ({ name } = { name: 'World' }) {
+function App ({ name } = { name: 'World' }) {
   return (
     <main id="root">
       <Heading name={name}/>
     </main>
   )
 }
+
+App.propTypes = {
+  name: PropTypes.string
+}
+
+export default App
--- src/js/components/heading.js
+++ src/js/components/heading.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-typs'
 
-export default function Heading ({ name }) {
+function Heading ({ name }) {
   return <h1>Hello, {name}!</h1>
 }
+
+Heading.propTypes = {
+  name: PropTypes.string
+}
+
+export default Heading
